7041249b26b98f94a820eee28c107fc0b5dd2bd3
김효준 <gywns41@todo-works.com>
2019-12-13 11:25:16 +0900

    Added Media setting

--- users/models.py
+++ users/models.py
@@ -28,7 +28,7 @@ class User(AbstractUser):
 
     CURRENT_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatras", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(null=True)
